354b17d66279a094d94bb8c5159d0ea174b99825
easrng <easrng@gmail.com>
2022-10-31 15:00:36 -0400

    spinny

--- src/routes/_static/animations.js
+++ src/routes/_static/animations.js
@@ -1,6 +1,17 @@
 export const FAVORITE_ANIMATION = [
   {
     properties: [
+      { transform: 'rotate(0deg)' },
+      { transform: 'rotate(180deg)' },
+      { transform: 'rotate(360deg)' }
+    ],
+    options: {
+      duration: 333,
+      easing: 'ease-in-out'
+    }
+  },
+  {
+    properties: [
       { transform: 'scale(1)' },
       { transform: 'scale(2)' },
       { transform: 'scale(1)' }
@@ -22,7 +33,29 @@ export const FAVORITE_ANIMATION = [
   }
 ]
 
-export const REBLOG_ANIMATION = FAVORITE_ANIMATION
+export const REBLOG_ANIMATION = [
+  {
+    properties: [
+      { transform: 'scale(1)' },
+      { transform: 'scale(2)' },
+      { transform: 'scale(1)' }
+    ],
+    options: {
+      duration: 333,
+      easing: 'ease-in-out'
+    }
+  },
+  {
+    properties: [
+      { fill: 'var(--action-button-fill-color)' },
+      { fill: 'var(--action-button-fill-color-pressed)' }
+    ],
+    options: {
+      duration: 333,
+      easing: 'linear'
+    }
+  }
+]
 
 export const FOLLOW_BUTTON_ANIMATION = [
   {
