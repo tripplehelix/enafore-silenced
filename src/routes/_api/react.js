b1cbfb0e4400d82affadd62eea5fbac0db1f041a
easrng <easrng@gmail.com>
2022-12-03 19:23:46 -0500

    pleroma/akkoma support

--- src/routes/_api/react.js
+++ src/routes/_api/react.js
@@ -1,15 +1,21 @@
-import { post, WRITE_TIMEOUT } from '../_utils/ajax.js'
+import { post, put, del, WRITE_TIMEOUT } from '../_utils/ajax.js'
 import { basename, auth } from './utils.js'
 
 export async function reactStatus (instanceName, accessToken, statusId, reactionName, apiVersion) {
-  if (apiVersion.isPleroma) {} else {
+  if (apiVersion.isPleroma) {
+    const url = `${basename(instanceName)}/api/v1/pleroma/statuses/${statusId}/reactions/${encodeURIComponent(reactionName)}`
+    return put(url, null, auth(accessToken), { timeout: WRITE_TIMEOUT })
+  } else {
     const url = `${basename(instanceName)}/api/v1/statuses/${statusId}/react/${encodeURIComponent(reactionName)}`
     return post(url, null, auth(accessToken), { timeout: WRITE_TIMEOUT })
   }
 }
 
 export async function unreactStatus (instanceName, accessToken, statusId, reactionName, apiVersion) {
-  if (apiVersion.isPleroma) {} else {
+  if (apiVersion.isPleroma) {
+    const url = `${basename(instanceName)}/api/v1/pleroma/statuses/${statusId}/reactions/${encodeURIComponent(reactionName)}`
+    return del(url, null, auth(accessToken), { timeout: WRITE_TIMEOUT })
+  } else {
     const url = `${basename(instanceName)}/api/v1/statuses/${statusId}/unreact/${encodeURIComponent(reactionName)}`
     return post(url, null, auth(accessToken), { timeout: WRITE_TIMEOUT })
   }
