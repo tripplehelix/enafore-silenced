e370b9da6787cb21c96a7d73d0d5905be44abae0
easrng <easrng@gmail.com>
2022-12-03 19:27:03 -0500

    fix

--- src/routes/_api/react.js
+++ src/routes/_api/react.js
@@ -14,7 +14,7 @@ export async function reactStatus (instanceName, accessToken, statusId, reaction
 export async function unreactStatus (instanceName, accessToken, statusId, reactionName, apiVersion) {
   if (apiVersion.isPleroma) {
     const url = `${basename(instanceName)}/api/v1/pleroma/statuses/${statusId}/reactions/${encodeURIComponent(reactionName)}`
-    return del(url, null, auth(accessToken), { timeout: WRITE_TIMEOUT })
+    return del(url, auth(accessToken), { timeout: WRITE_TIMEOUT })
   } else {
     const url = `${basename(instanceName)}/api/v1/statuses/${statusId}/unreact/${encodeURIComponent(reactionName)}`
     return post(url, null, auth(accessToken), { timeout: WRITE_TIMEOUT })
