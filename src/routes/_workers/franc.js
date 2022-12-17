0e84f3890e28250d48f67fdd54492ecfb73b48a2
easrng <easrng@gmail.com>
2022-12-17 12:49:13 -0500

    make the worker work

--- src/routes/_workers/franc.js
+++ src/routes/_workers/franc.js
@@ -1,7 +1,14 @@
 import { decompress } from 'fzstd'
 import registerPromiseWorker from 'promise-worker/register.js'
 self.decompress=decompress;
+const francAll = (async()=>{
+  const ab = await(await fetch("/franc-all.zst")).arrayBuffer()
+  const u8 = new Uint8Array(ab)
+  const d8 = decompress(u8)
+  const sr = new TextDecoder().decode(d8)
+  const sf = sr.replace(/export{.+/,"return{franc:K,francAll:B}")
+  return new Function(sf)().francAll
+})()
 registerPromiseWorker(async (text) => {
-  debugger
-  //return franc(text)
+  return (await francAll)(text)
 })
