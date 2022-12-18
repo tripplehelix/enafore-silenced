182e5177b35c6bfdf77f69ed2250bfd3ed7af974
easrng <easrng@gmail.com>
2022-12-18 06:43:47 -0500

    add nodeInfo to instanceInfo

--- src/routes/_api/instance.js
+++ src/routes/_api/instance.js
@@ -5,5 +5,22 @@ export function getInstanceInfo (instanceName, accessToken) {
   const url = `${basename(instanceName)}/api/v1/instance`
   // accessToken is required in limited federation mode, but elsewhere we don't need it (e.g. during login)
   const headers = accessToken ? auth(accessToken) : null
-  return get(url, headers, { timeout: DEFAULT_TIMEOUT })
+  const instance = await get(url, headers, { timeout: DEFAULT_TIMEOUT })
+  instance.nodeInfo = null
+  try{
+    const nodeInfo = await get(`${basename(instanceName)}/.well-known/nodeinfo`, headers, { timeout: DEFAULT_TIMEOUT })
+    let nodeInfo21, nodeInfo20;
+    for(let link of nodeInfo.links) {
+      if(link.rel === "http://nodeinfo.diaspora.software/ns/schema/2.1") {
+        nodeInfo21 = link.href
+      } else if(link.rel === "http://nodeinfo.diaspora.software/ns/schema/2.0") {
+        nodeInfo20 = link.href
+      }
+    }
+    const realNodeInfo = await get(nodeInfo21 || nodeInfo20, {}, { timeout: DEFAULT_TIMEOUT })
+    instance.nodeInfo = realNodeInfo;
+  } catch(e) {
+    console.warn("got error fetching nodeInfo", e)
+  }
+  return instance
 }
