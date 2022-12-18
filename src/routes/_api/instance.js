b4d1f1b75b0640f05e1e4d51b213c8f893cf6ebf
easrng <easrng@gmail.com>
2022-12-18 06:47:09 -0500

    forgot async (was returning a promise anyway)

--- src/routes/_api/instance.js
+++ src/routes/_api/instance.js
@@ -1,7 +1,7 @@
 import { get, DEFAULT_TIMEOUT } from '../_utils/ajax.js'
 import { auth, basename } from './utils.js'
 
-export function getInstanceInfo (instanceName, accessToken) {
+export async function getInstanceInfo (instanceName, accessToken) {
   const url = `${basename(instanceName)}/api/v1/instance`
   // accessToken is required in limited federation mode, but elsewhere we don't need it (e.g. during login)
   const headers = accessToken ? auth(accessToken) : null
