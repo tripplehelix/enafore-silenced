55a27a21c6a6bd2646fc09b8e21430f4c868c8a7
easrng <easrng@gmail.com>
2022-12-23 02:07:27 -0500

    fix

--- src/routes/_utils/emojifyText.js
+++ src/routes/_utils/emojifyText.js
@@ -9,7 +9,7 @@ export function emojifyText (text, emojis, autoplayGifs) {
     if(testEmojiSupported(substring)) {
       return `<span class="inline-emoji">${substring}</span>`
     } else {
-      return `<span class="inline-emoji unsupported" style="background-image:url('https://github.com/googlefonts/noto-emoji/blob/main/png/72/emoji_u${[...substring].map(e=>e.codePointAt(0).toString(16))}.png?raw=tru')">${substring}</span>`
+      return `<span class="inline-emoji unsupported" style="background-image:url('https://github.com/googlefonts/noto-emoji/blob/main/png/72/emoji_u${[...substring].map(e=>e.codePointAt(0).toString(16)).join("_")}.png?raw=tru')">${substring}</span>`
     }
   })
 
