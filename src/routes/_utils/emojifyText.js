e7fb4e6598f1aa3268e7342a8e0e8d73f7b15a06
easrng <easrng@gmail.com>
2022-12-25 22:18:31 -0500

    boop

--- src/routes/_utils/emojifyText.js
+++ src/routes/_utils/emojifyText.js
@@ -2,21 +2,26 @@ import { replaceAll } from './strings.js'
 import { replaceEmoji } from './replaceEmoji.js'
 import { testEmojiSupported } from './testEmojiSupported.js'
 
-const styleEle = document.createElement("style")
-styleEle.id = "theEmojiStyle"
-document.head.appendChild(styleEle)
+let styleEle;
+if(typeof window !== "undefined") {
+  styleEle = document.createElement("style")
+  styleEle.id = "theEmojiStyle"
+  document.head.appendChild(styleEle)
+  window.addEventListener("input", function (e) {
+    emojifyText(e.data+"")
+  })
+}
+
 const polyfilled = new Set()
 export function emojifyText (text, emojis, autoplayGifs) {
   // replace native emoji with wrapped spans so we can give them the proper font-family
   // as well as show tooltips
   text = replaceEmoji(text, substring => {
-    if(!testEmojiSupported(substring)) {
-      if(!polyfilled.has(substring)) {
-        polyfilled.add(substring);
-        (async () => {
-          styleEle.textContent += await(await fetch("https://fonts.googleapis.com/css2?family=Noto+Colr+Emoji+Glyf&text=" + encodeURIComponent(substring))).text()
-        })()
-      }
+    if(styleEle && !polyfilled.has(substring) && !testEmojiSupported(substring)) {
+      polyfilled.add(substring);
+      (async () => {
+        styleEle.textContent += await(await fetch("https://fonts.googleapis.com/css2?family=Noto+Colr+Emoji+Glyf&text=" + encodeURIComponent(substring))).text()
+      })()
     }
     return `<span class="inline-emoji">${substring}</span>`
   })
@@ -36,10 +41,4 @@ export function emojifyText (text, emojis, autoplayGifs) {
   }
 
   return text
-}
-
-if(typeof window !== "undefined") {
-  window.addEventListener("input", function (e) {
-    emojifyText(e.data+"")
-  })
 }
