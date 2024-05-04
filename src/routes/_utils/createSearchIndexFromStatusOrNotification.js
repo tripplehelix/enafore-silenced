3cdbb6cf807fe3aafc15904d557724894ec231cd
easrng <me@easrng.net>
2024-05-04 06:53:40 -0400

    fix #13

--- src/routes/_utils/createSearchIndexFromStatusOrNotification.js
+++ src/routes/_utils/createSearchIndexFromStatusOrNotification.js
@@ -1,18 +1,12 @@
 let domParser
 
 // copy-pasta'd from
-// https://github.com/tootsuite/mastodon/blob/2ff01f7/app/javascript/mastodon/actions/importer/normalizer.js#L58-L75
+// https://github.com/mastodon/mastodon/blob/b7902225d698a107df2cf8b4ca221caad38fa464/app/javascript/mastodon/actions/importer/normalizer.js#L65
 export const createSearchIndexFromStatusOrNotification = statusOrNotification => {
   const status = statusOrNotification.status || statusOrNotification // status on a notification
   const originalStatus = status.reblog || status
   domParser = domParser || new DOMParser()
   const spoilerText = originalStatus.spoiler_text || ''
-  const searchContent = ([spoilerText, originalStatus.content]
-    .concat(
-      (originalStatus.poll && originalStatus.poll.options)
-        ? originalStatus.poll.options.map(option => option.title)
-        : []
-    ))
-    .join('\n\n').replace(/<br\s*\/?>/g, '\n').replace(/<\/p><p>/g, '\n\n')
+  const searchContent = ([spoilerText, originalStatus.content].concat((originalStatus.poll && originalStatus.poll.options) ? originalStatus.poll.options.map(option => option.title) : [])).concat(originalStatus.media_attachments.map(att => att.description)).join('\n\n').replace(/<br\s*\/?>/g, '\n').replace(/<\/p><p>/g, '\n\n')
   return domParser.parseFromString(searchContent, 'text/html').documentElement.textContent
 }
